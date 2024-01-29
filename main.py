5a7181fbe020bd87c5b6fb72a87123ed87b77dcc
Sergii Franchuk <sergii.franchuk@starnavi.io>
2024-01-29 13:49:34 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from api import router as tasks_router
+from tortoise.contrib.fastapi import register_tortoise
+
+import settings
+from views import router as tasks_router
+from users.views import router as users_router
 
 
 application = FastAPI()
 application.include_router(tasks_router)
+application.include_router(users_router)
 
 
-# @application.post("/signup/")
-# async def sign_up(user: UserIn):
-#     use_case = SignUpUseCase()
-#     try:
-#         return await use_case(user.model_dump())
-#     except EmailError as error:
-#         return JSONResponse(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             content={"email": error.args},
-#         )
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:application", host="0.0.0.0", port=8000, log_level="info", reload=True
-    )
+register_tortoise(application, config=settings.TORTOISE_ORM_CONFIG)
